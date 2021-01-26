d39d172087b033a08308a12e8bc1ddee17c12bd8
TheChiragRathod <crathod116@rku.ac.in>
2021-01-26 10:13:29 +0530

    added port, start script, connection string into index.js file

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const mongoose = require('mongoose')
 const route = require("./route")
 
                                     //give database name here...
-mongoose.connect("mongodb://localhost:27017/Employee",{useNewUrlParser:true,useUnifiedTopology:true}).then(
+mongoose.connect("mongodb+srv://Chirag:Star@456@cluster0.thc18.mongodb.net/Employee?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true}).then(
     ()=>
     {
         const app=express();
         app.use("",route)
 
-        app.listen(3000,()=>{
+        app.listen(process.env.PORT || 3000,()=>{
             console.log("Server started!...")
         })
     }
